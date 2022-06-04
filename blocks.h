4d4044a32c0d7c023f9d141b3981eb04bd6e2157
Marc Partensky <marc.partensky@gmail.com>
2022-06-04 17:32:26 +0200

    icons + brightness

--- blocks.h
+++ blocks.h
@@ -1,7 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "echo $(cat /sys/class/power_supply/BAT0/capacity)%",	30,		0},
+    {" ", "brightnessctl  -m | sed 's/,/ /g' | awk '{print $4}'", 5, 0},
+	{" ", "echo $(pamixer --get-volume)%",	2,		0},
+	{" ", "echo $(cat /sys/class/power_supply/BAT0/capacity)%",	60,		0},
 	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	{"", "date '+%a %d %b %I:%M%p'",					5,		0},
 };
