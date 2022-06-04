d3ed491408b18828f8919e2aa9bd6f4b6888624e
Marc Partensky <marc.partensky@gmail.com>
2022-06-04 17:35:20 +0200

    date

--- blocks.h
+++ blocks.h
@@ -5,7 +5,7 @@ static const Block blocks[] = {
 	{" ", "echo $(pamixer --get-volume)%",	2,		0},
 	{" ", "echo $(cat /sys/class/power_supply/BAT0/capacity)%",	60,		0},
 	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"", "date '+%a %d %b %I:%M%p'",					5,		0},
+	{"", "date '+%a %d %b %H:%M '",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
