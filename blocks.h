d1a1af6b8bd150a5e95dc30ebaed14362b25ae50
Marc Partensky <marc.partensky@gmail.com>
2022-06-10 19:30:04 +0200

    list connected bluetooth devices

--- blocks.h
+++ blocks.h
@@ -5,6 +5,7 @@ static const Block blocks[] = {
     {" ", "brightnessctl  -m | sed 's/,/ /g' | awk '{print $4}'", 5, 0},
 	{" ", "echo $(pamixer --get-volume)%",	2,		0},
 	{" ", "echo $(cat /sys/class/power_supply/BAT0/capacity)%",	60,		0},
+    {"  ", "bluetoothctl info | head -n 2 | tail -n 1 | sed 's/Name: //' | xargs", 5, 0},
     {"  ", "iwgetid -r", 5, 0},
 	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	{"", "date '+%a %d %b %H:%M '",					5,		0},
